273b5d9a5404f966da45086d2061bfa1f4d9843d
jajimenez <joseajimenez@hola.com>
2023-06-05 19:16:47 +0200

    [PDF-003] AppProcess update Data Props from buttons

--- components/AppProcess/index.tsx
+++ components/AppProcess/index.tsx
@@ -21,34 +21,55 @@ const defaultProps = {
   ],
 };
 
-export const createDataPadf = () => {
+const createDataPadf = () => {
   localStorage.setItem("pdfContent", JSON.stringify(defaultProps));
   return localStorage.getItem("pdfContent");
 };
+
+const updateProcessData = (prop:string, value: string) => {
+  const storage:string = localStorage.getItem("pdfContent") || '';
+  const activeData = JSON.parse(storage);
+  activeData[prop] = value;
+  localStorage.setItem("pdfContent", JSON.stringify(activeData));
+};
+
+const onOptionChange = (e:any) => {
+  updateProcessData(e.target.name, e.target.value);
+}
+
 
 export const AppProcess: FC<AppProcessProps> = ({ label, type }) => {
   createDataPadf();
 
   return (
-    <div className="bg-sky-700/50 p-8 rounded-xl mb-8">
+    <div className="bg-sky-500/70 p-8 rounded-xl mb-8">
       <Fieldset
         legend="Tamaño  de página"
-        name="page_format"
+        name="pageType"
         type="radio"
         options={[
-          { value: "hola", label: "hola" },
-          { value: "adios", label: "adios" },
+          { value: "a4", label: "A4", checked: true },
+          { value: "a3", label: "A3" },
         ]}
+        onOptionChange={onOptionChange}
       />
       <Fieldset
         legend="Diseño de página"
-        name="page_theme"
+        name="pageTheme"
         type="radio"
         options={[
-          { value: "hola", label: "hola" },
-          { value: "adios", label: "adios" },
+          { value: "default", label: "Defautl", checked: true },
+          { value: "summer", label: "Summer" },
         ]}
+        onOptionChange={onOptionChange}
       />
+      <button
+        className="rounded-full px-8 py-3 uppercase text-sm text-sky-700 ring-1 ring-sky-700 
+      hover:bg-sky-700 hover:text-white hover:transition-all"
+        onClick={() => updateProcess('one')}
+      >
+        Siguiente
+      </button>
     </div>
   );
 };
--- components/AppUI/Fieldset/index.tsx
+++ components/AppUI/Fieldset/index.tsx
@@ -1,5 +1,5 @@
-import ReactPDF, { StyleSheet, Text } from "@react-pdf/renderer";
-import { FC } from "react";
+import { FC, useState } from "react";
+//import classes from "./Fieldset.module";
 
 export interface FieldsetProps {
   legend?: string;
@@ -8,23 +8,32 @@ export interface FieldsetProps {
   options: {
     value: string;
     label: string;
+    checked?: boolean | undefined;
   }[]
+  onOptionChange: (event: React.MouseEvent<HTMLElement>) => void;
 }
 
-export const Fieldset: FC<FieldsetProps> = ({ legend, name, type,  options }) => {
+export const Fieldset: FC<FieldsetProps> = ({ legend, name, type,  options, onOptionChange }) => {
+  
+  // const onOptionChange = (name: string, value: string) => {
+  //   console.log(`${name} : ${value}`)
+  // }
+
   return (
     <fieldset className="bg-sky-100/50 p-8 rounded-xl mb-8 flex">
       <legend className="text-sm font-semibold leading-6 text-gray-900">
         {legend}
       </legend>
       {options.map((option, index) => (
-        <div key={`optopn-${index}`} className="flex h-6 items-center">
+        <div key={`optopn-${index}`} className="flex h-6 items-center mx-4">
           <input
             id={option.value}
             value={option.value}
             name={name}
             type={type}
-            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+            checked={option.checked}
+            className="focus:ring-indigo-600 mr-2"
+            onClick={(e) => onOptionChange(e)}
           />
           <label htmlFor="comments" className="font-medium text-gray-900">
             {option.label}
