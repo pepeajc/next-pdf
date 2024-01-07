79080ec4e2780abc0dcea43f9a4b28f30b74fb0d
jajimenez <joseajimenez@hola.com>
2024-01-07 19:02:02 +0100

    Redir non www to www

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     profiler: true,
+    async redirects() {
+      return [
+        {
+          source: '/:path*',
+          has: [{ type: 'host', value: 'tools.rankios.com' }],
+          destination: 'https://www.tools.rankios.com/:path*',
+          permanent: true
+        },
+      ];
+    },
   }
   
   module.exports = nextConfig
